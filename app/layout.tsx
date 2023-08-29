b6c5ada816dddcfad3206bffc1d974824c91679c
dontrellthedeveloper <dontrellknight@gmail.com>
2023-08-28 21:39:14 -0400

    added create server modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Open_Sans } from "next/font/google";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from '@/components/providers/modal-provider'
 
 import { cn } from "@/lib/utils";
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="dark"
           >
+             <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
